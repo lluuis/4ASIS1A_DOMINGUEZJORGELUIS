696134acbe01ce4e88ae5798268035dac9658974
lluuis <j.dominguez.s083@edu.utc.mx>
2022-09-21 08:50:02 -0500

    entrada de texto

---
+++ 02holamundo/holamundo.java
@@ -0,0 +1,19 @@
+//esto es un comentario
+/*
+ * este es un comentario nos sirve para poder varias lineas de codigo
+ * 
+ * todo programa en java, maneja clases, una clase es un objeto del mundo real 
+*/
+
+class holamundo{
+    //cuerpo del programa 
+    /*
+     * aqui es donde nosotros codificamos todos los elementos del programa, funciones, estructuras, variables y metodos 
+     * toda clase debe tener un metodo principal 
+     */
+
+     public static void main(String[]args){
+        System.out.println("hola  mundo");
+
+     }
+}
